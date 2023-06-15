75ccc5e67cce7b8aa2e52fb0eed1aac398e776fb
giulianac-git <79769030+giulianac-git@users.noreply.github.com>
2023-06-15 02:28:54 +0000

    Final Version

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomWho = who[Math.round(Math.random() * (who.length - 1))];
+  let randomAction = action[Math.round(Math.random() * (action.length - 1))];
+  let randomWhat = what[Math.round(Math.random() * (what.length - 1))];
+  let randomWhen = when[Math.round(Math.random() * (when.length - 1))];
+  let excuse =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen + ".";
+
+  let excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = "OMG! You will not believe me but... <br>" + excuse;
 };
+
+onload();
